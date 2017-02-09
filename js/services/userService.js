5ff92c4cb946fabcd9ee0e5a91a2aa67c7eebe48
vinifig <vinifig@hotmail.com>
2017-02-09 16:00:16 -0200

    commit teste

--- js/services/userService.js
+++ js/services/userService.js
@@ -6,7 +6,21 @@ $(document).ready(function(){
     return new Promise((resolve, reject)=>{
       // DBService.insert(userKey, userData)
         // .then()
-    })
+    });
+  }
+
+  UserService.login = function(userData){
+    return new Promise((resolve, reject)=>{
+      // DBService.get(userKey, userData)
+        // .then(processaSenha)
+      resolve(userData);
+    });
+  }
+
+  UserService.edit = function(userData){
+    return new Promise((resolve, reject)=>{
+      
+    });
   }
 
   window.UserService = UserService;
